92e38ea3436943011014f8ccf77fb1234edc7862
Wade-Martin <wsmartin23@gmail.com>
2019-10-17 04:42:14 +1100

    css changes

--- src/AboutMe.svelte
+++ src/AboutMe.svelte
@@ -12,7 +12,7 @@
   onMount(() => {
     element = document.getElementById("container");
     getRect = element.getBoundingClientRect();
-    position = getRect.top - 300;
+    position = getRect.top - 100;
     console.log(position)
   })
 </script>
