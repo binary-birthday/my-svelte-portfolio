d18689a2f7bad0283a5f7c171fa3694ffe66726e
Wade-Martin <wsmartin23@gmail.com>
2019-10-17 04:17:19 +1100

    css changes

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -11,7 +11,6 @@
 
   onMount(() => {
     element = document.getElementById("background");
-    console.log(element)
     getRect = element.getBoundingClientRect();
     position = getRect.top;
   })
