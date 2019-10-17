7ab8d3e7862ca1ffa37bc95607ce8bd3bacb23cf
Wade-Martin <wsmartin23@gmail.com>
2019-10-17 14:09:44 +1100

    css changes

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -131,15 +131,15 @@
   <div class="left"></div>
   <div class="form-container">
     {#if y > position}
-    <h1 in:slide="{{delay: 300, duration: 1500, easing: quintOut }}" out:fade="{{delay: 0, duration: 1000}}" >Get In Touch</h1>
+    <h1 in:slide="{{delay: 500, duration: 1500, easing: quintOut }}" out:fade="{{delay: 0, duration: 1000}}" >Get In Touch</h1>
     <form action="https://formspree.io/wsmartin23@gmail.com" method="POST">      
       <p class="dn">
         <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
       </p>
-      <input in:scale="{{duration: 1500, delay: 350, opacity: 0, start: .75, easing:elasticOut}}" out:fade="{{delay: 0, duration: 1000}}" name="name" type="text" class="form-input" placeholder="Name" />   
-      <input in:scale="{{duration: 1500, delay: 400, opacity: 0, start: .75, easing:elasticOut}}" out:fade="{{delay: 0, duration: 1000}}" name="email" type="text" class="form-input" placeholder="Email" />
-      <textarea in:scale="{{duration: 1500, delay: 450, opacity: 0, start: .75, easing:elasticOut}}" out:fade="{{delay: 0, duration: 1000}}" name="text" class="form-input" placeholder="Your Message here..."></textarea>
-      <input in:scale="{{duration: 1500, delay: 500, opacity: 0, start: .75, easing:elasticOut}}" out:fade="{{delay: 0, duration: 1000}}" type="submit" value="SUBMIT"/>
+      <input in:scale="{{duration: 1500, delay: 550, opacity: 0, start: .75, easing:elasticOut}}" out:fade="{{delay: 0, duration: 1000}}" name="name" type="text" class="form-input" placeholder="Name" />   
+      <input in:scale="{{duration: 1500, delay: 600, opacity: 0, start: .75, easing:elasticOut}}" out:fade="{{delay: 0, duration: 1000}}" name="email" type="text" class="form-input" placeholder="Email" />
+      <textarea in:scale="{{duration: 1500, delay: 650, opacity: 0, start: .75, easing:elasticOut}}" out:fade="{{delay: 0, duration: 1000}}" name="text" class="form-input" placeholder="Your Message here..."></textarea>
+      <input in:scale="{{duration: 1500, delay: 700, opacity: 0, start: .75, easing:elasticOut}}" out:fade="{{delay: 0, duration: 1000}}" type="submit" value="SUBMIT"/>
     </form> 
     {/if}
   </div>
