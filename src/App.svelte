23dca9cb1ca39ff4a40fcef12f10b188ab08e244
Wade-Martin <wsmartin23@gmail.com>
2019-10-17 18:34:44 +1100

    added window height var from binding for main app wrapper div, adjusted all component width & height css to %

--- src/App.svelte
+++ src/App.svelte
@@ -8,27 +8,26 @@
 	let scrollPos;
 	let windowHeight;
 	let windowWidth;
-	$: width = windowWidth;
-	$: height = windowHeight;
 
 
 	const handleResize = () => {
-    let vh = windowHeight * 0.01;
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
+		document.documentElement.style.setProperty('--vh', `${windowHeight}px`);
 		console.log(windowHeight)
-  };
+	};
 	
-	let vh = windowHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty('--vh', `${windowHeight}px`);
 
 </script>
 
 <style>
-
+	div {
+		height: var(--vh);
+		width: 100vw;
+	}
 </style>
 
-<svelte:window bind:scrollY={scrollPos} bind:innerHeight={windowHeight} bind:innerWidth={windowWidth} on:resize={handleResize}/>
-<div {width} {height}>
+<svelte:window bind:scrollY={scrollPos} bind:innerHeight={windowHeight} on:resize={handleResize}/>
+<div>
 	<NavBar />
 	<Greeting />
 	<AboutMe scrollPos={scrollPos}/>
